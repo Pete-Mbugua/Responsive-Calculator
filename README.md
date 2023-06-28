1871d9ab2f8422bca07c761f7f1b688016b68cc7
stellah660 <valenntinestellah165@gmail.com>
2023-06-28 22:31:48 +0300

    First JS Project:  Calculator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+For my first JS Project, I made a calculator.
 
+As usual, the xpectations were high but the product...Lol
+
+I'll keep improving it in the subsequent releases...Lol
+
+I have used w3.css framework for css.
--- function.js
+++ function.js
@@ -1,1 +1,19 @@
+function appendToResult(value) {
+  var result = document.getElementById("result");
+  result.value += value;
+}
 
+function clearResult() {
+  var result = document.getElementById("result");
+  result.value = "";
+}
+
+function calculate() {
+  var result = document.getElementById("result");
+  try {
+    var calculation = eval(result.value);
+    result.value = calculation;
+  } catch (error) {
+    result.value = "Error";
+  }
+}
